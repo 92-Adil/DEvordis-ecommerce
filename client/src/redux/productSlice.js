fa7629cc9234ca7b257c86900e0142d7c7c3dbaf
92-adil <madilrehman04@gmail.com>
2025-05-06 13:43:10 +0500

    All most all Features Done

--- client/src/redux/productSlice.js
+++ client/src/redux/productSlice.js
@@ -5,12 +5,23 @@ export const fetchAllProducts = createAsyncThunk(
   "fetchAllProducts",
   async () => {
     try {
-      const res =await api.get("product/getProducts");
-    //   console.log("API response in thunk:", res.data);
+      const res = await api.get("product/getProducts");
+      //   console.log("API response in thunk:", res.data);
       return res.data.products;
     } catch (error) {
       console.log("Error in the the fectchAllProduct thunk", error);
-      throw error
+      throw error;
+    }
+  }
+);
+export const fectchProductById = createAsyncThunk(
+  "fectchProductById",
+  async (productId) => {
+    try {
+      const res = await api.get(`product/getProductById/${productId}`);
+      return res.data.product;
+    } catch (error) {
+      console.log("error in fectchSingleProduct thunk", error);
     }
   }
 );
@@ -21,6 +32,7 @@ const productSlice = createSlice({
     isLoading: false,
     products: [],
     isError: false,
+    product: null,
   },
   extraReducers: (builder) => {
     builder.addCase(fetchAllProducts.pending, (state) => {
@@ -34,6 +46,18 @@ const productSlice = createSlice({
       console.log("Error", action.payload);
 
       state.isError = true;
+    });
+    builder.addCase(fectchProductById.pending, (state) => {
+      state.isLoading = true;
+      state.isError = null;
+    });
+    builder.addCase(fectchProductById.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.product = action.payload;
+    });
+    builder.addCase(fectchProductById.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = action.payload;
     });
   },
   // reducers:{
